b9a99ba03971ce8eafb61051f0c240d7cd33b3be
mrivera96 <jylrivera96@gmail.com>
2021-01-12 13:49:14 -0600

    Ajustes para montar en servidor

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,22 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
 
     Route::group(['middleware' => 'auth:api'], function() {
-        Route::get('logout', 'AuthController@logout');
+        Route::post('logout', 'AuthController@logout');
     });
 });
 
 Route::group(['prefix' => 'vehiculos'], function(){
    Route::group(['middleware' => 'auth:api'], function(){
-     Route::get('buscar', 'VehiculosController@buscarVehiculo');
-     Route::get('getDetalle', 'VehiculosController@getDetalleVehiculo');
-     Route::get('getTipos', 'VehiculosController@getTipos');
-     Route::get('getTanques', 'VehiculosController@getTanquesComb');
+     Route::post('buscar', 'VehiculosController@buscarVehiculo');
+     Route::post('getDetalle', 'VehiculosController@getDetalleVehiculo');
+     Route::post('getTipos', 'VehiculosController@getTipos');
+     Route::post('getTanques', 'VehiculosController@getTanquesComb');
    });
 });
 
 Route::group(['prefix' => 'agencias'], function (){
     Route::group(['middleware' => 'auth:api'], function (){
-        Route::get('listar', 'AgenciasController@listar');
+        Route::post('listar', 'AgenciasController@listar');
     });
 });
 
@@ -44,12 +44,12 @@ Route::group(['prefix' => 'inspecciones'], function (){
 
 Route::group(['prefix' => 'usuarios'], function(){
     Route::group(['middleware' => 'auth:api'], function(){
-        Route::get('listar', 'UsuariosController@listarUsuarios');
+        Route::post('listar', 'UsuariosController@listarUsuarios');
     });
 });
 
 Route::group(['prefix' => 'accesorios'], function (){
     Route::group(['middleware' => 'auth:api'], function (){
-        Route::get('listar', 'AccesoriosController@listar');
+        Route::post('listar', 'AccesoriosController@listar');
     });
 });
