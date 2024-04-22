55fe55e4e0267e283c4374fc60904f9a27cc26bd
mriveraz29 <jylrivera96@gmail.com>
2024-04-22 15:48:22 -0600

    se agrego cors para localhost

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://inspecciones.test','https://localhost','*'],
+    'allowed_origins' => ['http://inspecciones.test','https://localhost','http://localhost','*'],
 
     'allowed_origins_patterns' => [],
 
