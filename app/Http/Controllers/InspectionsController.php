196554b41d37434c29c416d41a8e78dae7ff2278
mriveraz29 <jylrivera96@gmail.com>
2024-05-22 15:01:04 -0600

    aplicacion de cambios para envio de correo pdf

--- app/Http/Controllers/InspectionsController.php
+++ app/Http/Controllers/InspectionsController.php
@@ -19,7 +19,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Auth;
-use Barryvdh\DomPDF\PDF;
+use PDF;
 use Illuminate\Support\Facades\Mail;
 
 
